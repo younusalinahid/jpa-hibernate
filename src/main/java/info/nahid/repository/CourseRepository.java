550fe4635984a62df1a26748b83f8373d11f6204
nahid <eng.yanahid@gmail.com>
2023-10-15 22:13:29 +0600

    JPQL Basic

--- src/main/java/info/nahid/repository/CourseRepository.java
+++ src/main/java/info/nahid/repository/CourseRepository.java
@@ -4,8 +4,11 @@ import info.nahid.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
+    List<Course> findCourseByNameIsLike(String s);
 
 }
---
+++ src/test/java/info/nahid/repository/JPQLTest.java
@@ -0,0 +1,30 @@
+package info.nahid.repository;
+
+import info.nahid.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.List;
+
+@DataJpaTest
+public class JPQLTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    CourseRepository repository;
+
+    @Test
+    public void jpql_basic() {
+        List<Course> courses = repository.findAll();
+        logger.info("Select c From Course c -> {}", courses);
+    }
+
+    @Test
+    public void jpql_where() {
+        List<Course> courses = repository.findCourseByNameIsLike("%100 Steps");
+        logger.info("Select c From Course c where name like '%100 Steps' -> {}", courses);
+    }
+}
